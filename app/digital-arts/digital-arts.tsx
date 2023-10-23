02e98856ee7484b377eb4f3d353aaecd205507a0
Talha Kerpicci <talha.kerpicci@gmail.com>
2023-10-24 00:34:18 +0300

    Add view images in full screen

---
+++ app/digital-arts/digital-arts.tsx
@@ -0,0 +1,135 @@
+'use client';
+import Image from "next/image";
+import { Metadata } from "next";
+import { useState, useEffect } from 'react';
+import { BiRightArrowAlt, BiLeftArrowAlt } from "react-icons/bi";
+import CloseIcon from "@/app/components/shared/CloseIcon";
+import { images } from "../data/data";
+import { Slide } from "../animation/Slide";
+
+export const metadata: Metadata = {
+    title: "Digital Arts | Talha Kerpicci",
+    metadataBase: new URL("https://talhakerpicci.com/digital-arts"),
+    description: "Explore digital arts images by Talha Kerpicci",
+    openGraph: {
+        title: "Digital Arts | Talha Kerpicci",
+        url: "https://talhakerpicci.com/digital-arts",
+        description: "Explore digital arts by Talha Kerpicci",
+        images: "https://raw.githubusercontent.com/talhakerpicci/talhakerpicci.com/main/public/images/illustrations/digital-arts.png",
+    },
+};
+
+export const imgs = Array.from({ length: images }, (_, index) => ({
+    id: index,
+    src: `/images/digital-arts/${index}.png`
+}));
+
+export default function DigitalArts() {
+    const [lightboxOpen, setLightboxOpen] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [opacity, setOpacity] = useState(1);
+
+    function openLightbox(index: number) {
+        setCurrentImageIndex(index);
+        setLightboxOpen(true);
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeLightbox() {
+        setLightboxOpen(false);
+        document.body.style.overflow = '';
+    }
+
+    function changeImage(direction: 'next' | 'prev') {
+        setOpacity(0);
+        setTimeout(() => {
+            if (direction === 'next') {
+                setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imgs.length);
+            } else {
+                setCurrentImageIndex((prevIndex) => (prevIndex - 1 + imgs.length) % imgs.length);
+            }
+            setOpacity(1);
+        }, 300);
+    }
+
+    function nextImage() {
+        changeImage('next');
+    }
+
+    function prevImage() {
+        changeImage('prev');
+    }
+
+    useEffect(() => {
+        const handleKeydown = (e: any) => {
+            if (e.key === 'Escape') {
+                closeLightbox();
+            }
+        };
+
+        document.addEventListener('keydown', handleKeydown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeydown);
+        };
+    }, []);
+
+    return (
+        <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
+            <div className="lg:max-w-2xl max-w-2xl">
+                <Slide>
+                    <h1 className="font-incognito font-black tracking-tight text-3xl sm:text-5xl mb-3 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
+                        Digital Arts
+                    </h1>
+                    <p className="text-base dark:text-zinc-400 text-zinc-600 leading-relaxed">
+                        Explore a collection of {imgs.length} digital creations, made with DALL·E.
+                    </p>
+                </Slide>
+            </div>
+            <figure className="my-6">
+                <Slide delay={0.12} className="flex flex-wrap gap-8">
+                    {imgs.slice().reverse().map((image) => (
+                        <div
+                            key={image.id}
+                            className="image-container dark:bg-primary-bg bg-secondary-bg"
+                            style={{ width: '350px', height: '350px' }}
+                        >
+                            <Image
+                                src={image.src}
+                                alt="Digital Art"
+                                layout="fill"
+                                objectFit="cover"
+                                onClick={() => openLightbox(image.id)}
+                            />
+                        </div>
+                    ))}
+                </Slide>
+            </figure>
+
+            {lightboxOpen && (
+                <div
+                    className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+                    onClick={closeLightbox}
+                >
+                    <div className="absolute left-4 top-4 text-white text-xl">
+                        {currentImageIndex + 1}/{imgs.length}
+                    </div>
+                    <div className="absolute left-4 top-1/2 transform -translate-y-1/2" onClick={(e) => e.stopPropagation()}>
+                        <BiLeftArrowAlt size={50} onClick={nextImage} className="text-white text-3xl cursor-pointer" />
+                    </div>
+                    <img
+                        style={{ opacity: opacity, maxWidth: '90vw', maxHeight: '90vh' }}
+                        src={imgs[currentImageIndex].src}
+                        alt="Digital Art"
+                        className="rounded-xl transition-opacity duration-300"
+                        onClick={(e) => e.stopPropagation()}
+                    />
+                    <div className="absolute right-4 top-1/2 transform -translate-y-1/2" onClick={(e) => e.stopPropagation()}>
+                        <BiRightArrowAlt size={50} onClick={prevImage} className="text-white text-3xl cursor-pointer" />
+                    </div>
+                    <CloseIcon size={40} onClick={closeLightbox} className="absolute top-4 right-4 text-white text-3xl cursor-pointer" />
+                </div>
+            )}
+        </main>
+    );
+}
--- app/digital-arts/page.tsx
+++ app/digital-arts/page.tsx
@@ -1,7 +1,5 @@
-import { Slide } from "../animation/Slide";
-import Image from "next/image";
 import { Metadata } from "next";
-import { images } from "../data/data";
+import DigitalArts from "./digital-arts";
 
 export const metadata: Metadata = {
     title: "Digital Arts | Talha Kerpicci",
@@ -15,42 +13,10 @@ export const metadata: Metadata = {
     },
 };
 
-export const imgs = Array.from({ length: images }, (_, index) => ({
-    id: index,
-    src: `/images/digital-arts/${index}.png`
-}));
-
-export default function DigitalArts() {
+export default function Page() {
     return (
-        <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
-            <div className="lg:max-w-2xl max-w-2xl">
-                <Slide>
-                    <h1 className="font-incognito font-black tracking-tight text-3xl sm:text-5xl mb-3 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
-                        Digital Arts
-                    </h1>
-                    <p className="text-base dark:text-zinc-400 text-zinc-600 leading-relaxed">
-                        Explore a collection of {imgs.length} digital creations, made with DALL·E.
-                    </p>
-                </Slide>
-            </div>
-            <figure className="my-6">
-                <Slide delay={0.12} className="flex flex-wrap gap-8">
-                    {imgs.slice().reverse().map((image) => (
-                        <div
-                            key={image.id}
-                            className="image-container dark:bg-primary-bg bg-secondary-bg"
-                            style={{ width: '350px', height: '350px' }}
-                        >
-                            <Image
-                                src={image.src}
-                                alt="Digital Art"
-                                layout="fill"
-                                objectFit="cover"
-                            />
-                        </div>
-                    ))}
-                </Slide>
-            </figure>
-        </main>
+        <div>
+            <DigitalArts />
+        </div>
     );
 }
