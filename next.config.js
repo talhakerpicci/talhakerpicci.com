b0836f1f15055f8da71267f5e7fcec0c88475260
Talha Kerpicci <talha.kerpicci@gmail.com>
2025-09-30 15:47:42 +0300

    Enhance Next.js configuration with output export, unoptimized images, and trailing slash support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { hostname: "raw.githubusercontent.com" },
       { hostname: "www.google.com" },
     ],
   },
+  trailingSlash: true,
 };
+
+module.exports = nextConfig;
